de66a79a7e8d07e2677b8eb8b959d582a4d564c7
Shikhar Shah <sshah369@login-phoenix-slurm-2.pace.gatech.edu>
2024-06-09 13:26:56 -0400

    Removed Mp output; removed account info from slurm scripts

--- RPA/src/cheFSI.c
+++ RPA/src/cheFSI.c
@@ -363,7 +363,7 @@ double estimate_initialError(SPARC_OBJ *pSPARC, RPA_OBJ *pRPA, int qptIndex, int
         #ifdef DEBUG
         t1 = MPI_Wtime();
         #endif
-        YT_multiply_Y_gamma(pRPA, pSPARC->dmcomm, pRPA->deltaVs, pSPARC->Nd_d_dmcomm, pSPARC->Nspinor_eig, flagNoDmcomm, 1);
+        YT_multiply_Y_gamma(pRPA, pSPARC->dmcomm, pRPA->deltaVs, pSPARC->Nd_d_dmcomm, pSPARC->Nspinor_eig, flagNoDmcomm, 0);
         MPI_Barrier(pRPA->nuChi0EigsBridgeComm);
         #ifdef DEBUG
         t2 = MPI_Wtime();
